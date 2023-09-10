b24d31b3cdc7e022a9a0f7c9ca93004a8a0844c5
Mello47 <melvinshuster47@gmail.com>
2023-09-10 10:47:12 +0200

    Update Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -16,27 +16,23 @@ const Services = () => {
           <ul className="service__list">
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Conduct user research to understand the target audience, their needs, behaviors, and preferences.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Develop wireframes to outline the basic structure and layout of a digital product, focusing on content placement and navigation.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Create interactive prototypes that allow stakeholders to test and provide feedback on the user interface and user experience.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/> 
+              <p>Design visually appealing user interfaces that align with brand guidelines and user expectations.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Continuously gather user feedback and insights to refine and improve the user experience.</p>
             </li>
           </ul>
         </article>
@@ -48,27 +44,27 @@ const Services = () => {
           <ul className="service__list">
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Design and build websites from scratch, tailored to the specific needs and goals of clients.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Provide end-to-end development services, handling both front-end and back-end development.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Optimize website speed and performance through techniques like code optimization, image compression, and caching.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Implement security best practices, such as encryption, authentication, and protection against common vulnerabilities (e.g., SQL injection, XSS)</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Integrate payment gateways, shopping carts, and inventory management systems.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Offer ongoing maintenance services, including updates, bug fixes, and security patches.</p>
             </li>
           </ul>
         </article>
@@ -80,27 +76,23 @@ const Services = () => {
           <ul className="service__list">
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Develop a content strategy that aligns with the client's goals and target audience.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Write high-quality articles, blog posts, social media updates, and other written content.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Optimize written content for search engines (SEO) to improve visibility and ranking in search results.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Monitor content performance using analytics tools to track engagement, reach, and conversion metrics.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Explore opportunities to monetize content, such as through affiliate marketing, sponsored posts, or selling digital products.</p>
             </li>
           </ul>
         </article>
